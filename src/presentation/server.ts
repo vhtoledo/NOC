868e2c3eb9763b2203c8655e03f15d591a20ac80
victor toledo <querynetworking@gmail.com>
2023-11-07 22:49:06 -0300

    creacion del servidor de presentation/cron/cron-service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,19 @@
+import { CronService } from "./cron/cron-service";
 
 
 export class Server {
 
     public static start() {
 
-        console.log("Server started...")
+        console.log("Server started...");
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('5 seconds', date);
+            }
+        );
+
     }
 }
