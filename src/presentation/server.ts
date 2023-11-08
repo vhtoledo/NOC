e12d8558700ee1ad4e6f468228561ad3e8b90096
victor toledo <querynetworking@gmail.com>
2023-11-07 23:11:49 -0300

    creacion de caso de uso check/check-service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 
@@ -10,8 +11,8 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *',
             () => {
-                const date = new Date();
-                console.log('5 seconds', date);
+
+                new CheckService().execute('https://google.com')
             }
         );
 
