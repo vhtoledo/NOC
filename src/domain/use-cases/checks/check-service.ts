3f2b6e68b16db4ad3b481a827301db037b337e0f
victor toledo <querynetworking@gmail.com>
2023-11-08 22:10:15 -0300

    refactor en caso de uso check-service

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,44 +1,49 @@
 import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
 import { LogRepository } from '../../repository/log.repository';
+
 interface CheckServiceUseCase {
-    execute( url: string ):Promise<boolean>;
+  execute( url: string ):Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = ( error: string ) => void;
+
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = (( error: string ) => void) | undefined;
+
+
+
 
 export class CheckService implements CheckServiceUseCase {
 
-    constructor(
-        private readonly logRepository: LogRepository,
-        private readonly successCallback: SuccessCallback,
-        private readonly errorCallback: ErrorCallback
-    ){}
+  constructor(
+    private readonly logRepository: LogRepository,
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
 
-    public async execute( url: string ): Promise<boolean> {
 
-        try {
+  public async execute( url: string ): Promise<boolean> {
 
-            const req = await fetch( url );
-            if ( !req.ok ) {
-                throw new Error( `Error on check service ${ url }` );
-            }
+    try {
+      const req = await fetch( url );
+      if ( !req.ok ) {
+        throw new Error( `Error on check service ${ url }` );
+      }
 
-            const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low );
-            this.logRepository.saveLog( log );
-            this.successCallback();
-            return true;
-            
-        } catch (error) {
-            const errorMessage = `${url} is not ok. ${ error }`;
-            const log = new LogEntity( errorMessage , LogSeverityLevel.high );
-            this.logRepository.saveLog(log);
-            
-            this.errorCallback && this.errorCallback( errorMessage );
+      const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low );
+      this.logRepository.saveLog( log );
+      this.successCallback && this.successCallback();
+
+      return true;
+    } catch (error) {
+      const errorMessage = `${url} is not ok. ${ error }`;
+      const log = new LogEntity( errorMessage , LogSeverityLevel.high );
+      this.logRepository.saveLog(log);
       
-            return true;
+      this.errorCallback && this.errorCallback( errorMessage );
 
-        }
-
+      return false;
     }
+
+  }
+
 }
