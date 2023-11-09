29d0bb43ab94b558a53f9d22caefb6279fb3a3cf
victor toledo <querynetworking@gmail.com>
2023-11-08 22:03:23 -0300

    implementacion en presentation/server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasources";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDataSource(),
+  );
 
 export class Server {
 
@@ -13,9 +18,11 @@ export class Server {
             () => {
                 const url = 'https://google.com';
                 new CheckService(
+                    fileSystemLogRepository,
                     () => console.log(`${url} is ok`),
                     (error) => console.log(error)
                 ).execute(url)
+                // new CheckService().execute( 'http://localhost:3000' );
             }
         );
 
