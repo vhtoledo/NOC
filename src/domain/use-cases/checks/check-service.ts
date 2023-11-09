f26c84a25cd8f2c478e0f391860f5dfed62bb1b7
victor toledo <querynetworking@gmail.com>
2023-11-08 21:55:14 -0300

    inyectar repositorio en caso de uso check-service

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,5 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
 interface CheckServiceUseCase {
     execute( url: string ):Promise<boolean>;
 }
@@ -8,6 +10,7 @@ type ErrorCallback = ( error: string ) => void;
 export class CheckService implements CheckServiceUseCase {
 
     constructor(
+        private readonly logRepository: LogRepository,
         private readonly successCallback: SuccessCallback,
         private readonly errorCallback: ErrorCallback
     ){}
@@ -21,14 +24,18 @@ export class CheckService implements CheckServiceUseCase {
                 throw new Error( `Error on check service ${ url }` );
             }
 
+            const log = new LogEntity(`Service ${ url } working`, LogSeverityLevel.low );
+            this.logRepository.saveLog( log );
             this.successCallback();
             return true;
             
         } catch (error) {
+            const errorMessage = `${url} is not ok. ${ error }`;
+            const log = new LogEntity( errorMessage , LogSeverityLevel.high );
+            this.logRepository.saveLog(log);
             
-            console.log(`${error}`)
-
-            this.errorCallback(`${error}`)
+            this.errorCallback && this.errorCallback( errorMessage );
+      
             return true;
 
         }
