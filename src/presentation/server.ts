83eb8df339cc54c70a462596a3dadad2fe0e7136
victor toledo <querynetworking@gmail.com>
2023-11-14 00:18:15 -0300

    creacion de postgresLog Datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -3,13 +3,15 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasources";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasources";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from './email/email.service';
 
 const logRepository = new LogRepositoryImpl(
     //new FileSystemDataSource(),
-    new MongoLogDatasource(),
+    //new MongoLogDatasource(),
+    new PostgresLogDatasource()
   );
   const emailService = new EmailService();
 
@@ -35,18 +37,18 @@ export class Server {
         //const logs = await logRepository.getLogs(LogSeverityLevel.medium);
         //console.log(logs);
 
-        //CronService.createJob(
-        //    '*/5 * * * * *',
-        //    () => {
-        //        const url = 'https://google.com';
-        //        new CheckService(
-        //            logRepository,
-        //            () => console.log(`${url} is ok`),
-        //            (error) => console.log(error)
-        //        ).execute(url)
-        //        // new CheckService().execute( 'http://localhost:3000' );
-        //    }
-        //);
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                new CheckService(
+                    logRepository,
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
+                ).execute(url)
+                // new CheckService().execute( 'http://localhost:3000' );
+            }
+        );
 
     }
 }
