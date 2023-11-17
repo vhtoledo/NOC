2701a4a1009cbe7a541ccf6289498a4c87f5e1a4
victor toledo <querynetworking@gmail.com>
2023-11-16 22:47:13 -0300

    montar bases de datos y variables de entorno para testing

---
+++ src/data/mongo/init.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+interface ConnectionOptions {
+    mongoUrl: string;
+    dbName: string;
+}
+
+export class MongoDataBase {
+
+    static async connect( options: ConnectionOptions ) {
+        const { mongoUrl, dbName } = options;
+
+        try {
+          
+          await mongoose.connect( mongoUrl, {
+            dbName: dbName,
+          });
+
+          console.log('Mongo connected');
+            
+        } catch (error) {
+            console.log('Mongo connection error');
+            throw error;
+        }
+    }
+}
