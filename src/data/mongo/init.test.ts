7de3d925e2e56d50d620cc78d604149d18aeebcb
victor toledo <querynetworking@gmail.com>
2023-11-17 01:11:30 -0300

    prueba en modelo de mongo

--- src/data/mongo/init.test.ts
+++ src/data/mongo/init.test.ts
@@ -1,36 +1,28 @@
 import mongoose from "mongoose";
-import { MongoDataBase } from "./init"
+import { MongoDataBase } from "./init";
 
+describe("init MongoDB", () => {
+  //Cerrar conexion de moongose
+  afterAll(() => {
+    mongoose.connection.close();
+  });
 
-describe('init MongoDB', () => {
-
-    //Cerrar conexion de moongose
-    afterAll(() => {
-        mongoose.connection.close();
-    })
-
-    test('should connect to MongoDB', async() => {
-
-        const connected = await MongoDataBase.connect({
-            dbName: process.env.MONGO_DB_NAME!,
-            mongoUrl: process.env.MONGO_URL!,
-        })
-
-        expect(connected).toBe(true);
+  test("should connect to MongoDB", async () => {
+    const connected = await MongoDataBase.connect({
+      dbName: process.env.MONGO_DB_NAME!,
+      mongoUrl: process.env.MONGO_URL!,
     });
 
-    test('should throw an error', async() => {
+    expect(connected).toBe(true);
+  });
 
-        try {
-
-            const connected = await MongoDataBase.connect({
-                dbName: process.env.MONGO_DB_NAME!,
-                mongoUrl: 'mongodb://victor:123456@localhostasdf:27017,'
-            });
-            expect(true).toBe(false);
-            
-        } catch (error) {
-            
-        }
-    })
-})
+  test("should throw an error", async () => {
+    try {
+      const connected = await MongoDataBase.connect({
+        dbName: process.env.MONGO_DB_NAME!,
+        mongoUrl: "mongodb://victor:123456@localhostasdf:27017,",
+      });
+      expect(true).toBe(false);
+    } catch (error) {}
+  });
+});
